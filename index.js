513dd7a4972f1bb5c8903cc50e52a87c8b76d12b
bradleyjosephh <bradleyjosephh@gmail.com>
2021-12-07 19:12:29 -0800

    index.js terminal displaying results

--- index.js
+++ index.js
@@ -4,56 +4,65 @@ const fs = require("fs");
 
 // array of questions for user
 const questions = [
-  {
-    type: 'input',
-    name: 'projectName',
-    message: "The title of your project: ",
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: "Provide a description of your project:",
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'Provide the installation instructions:',
-  },
-  {
-    type: 'input',
-    name: 'projectUse',
-    message: 'Provide instructions and examples so users/contributors can use the project:',
-  },
-  {
-    type: 'input',
-    name: 'credits',
-    message: 'If you worked on the project as a team or an organization, list your collaborators/team members:',
-  },
-  {
-    type: 'input',
-    name: 'license',
-    message: 'List the license information:',
-  },
-  {
-    type: 'input',
-    name: 'githubUsername',
-    message: 'Provide your Github Username:',
-  },
-  {
-    type: 'input',
-    name: 'emailAddress',
-    message: 'Provide your email address:',
-  }
+    {
+        type: 'input',
+        name: 'projectName',
+        message: "The title of your project: ",
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Provide a description of your project:",
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide the installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'projectUse',
+        message: 'Provide instructions and examples so users/contributors can use the project:',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'If you worked on the project as a team or an organization, list your collaborators/team members:',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'List the license information:',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Provide your Github Username:',
+    },
+    {
+        type: 'input',
+        name: 'emailAddress',
+        message: 'Provide your email address:',
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    
+
 }
 
 // function to initialize program
 function init() {
-
+    /* Pass your questions in here */
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+        })
+        .catch((error) => {
+            if (error) {
+                console.log(error)
+            }
+       })
 }
 
 // function call to initialize program
